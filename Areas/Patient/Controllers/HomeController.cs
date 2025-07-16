f9a9c69469c16d2bf9dd2fe6d229862137e5dc9d
ryan <ryanwhytejamaica@gmail.com>
2025-07-15 20:16:47 -0500

    Add project files.

---
+++ Areas/Patient/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KPH_Management_System.Areas.Patient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
